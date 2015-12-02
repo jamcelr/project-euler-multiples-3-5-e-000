0108c3fac4d0e3fc863e752e95dd9c80a27c0c22
jamcelr <jamcelr@hotmail.com>
2015-12-02 19:19:34 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  list = []
+  for i in 1..(limit.to_i-1)
+    if i % 3 == 0 || i % 5 == 0
+      list << i
+    end
+  end
+  return list
+end
+
+def sum_multiples(limit)
+  list = 0
+  for i in 1..(limit.to_i-1)
+    if i % 3 == 0 || i % 5 == 0
+      list += i 
+    end
+  end
+  return list
+end  
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    list = []
+    for i in 1..(@limit.to_i-1)
+      if i % 3 == 0 || i % 5 == 0
+        list << i
+      end
+    end
+    return list
+  end
+
+  def sum_multiples
+    list = 0
+    for i in 1..(@limit.to_i-1)
+      if i % 3 == 0 || i % 5 == 0
+        list += i
+      end
+    end
+    return list
+  end
+end
